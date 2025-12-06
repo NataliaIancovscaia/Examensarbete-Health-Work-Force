c98331fd5795c2c3a8b5315acf121f327c36d9b7
NataliaIancovscaia <Natalia.Iancovscaia@medieinstitutet.se>
2025-12-06 16:47:53 +0100

    Redeploy

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import serverless from "serverless-http";
+
 import Sentry from "./config/instrument.js";
 import connectDB from "./config/db.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
@@ -11,23 +12,26 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.get("/", (req, res) => {
   console.log("Root route called");
   res.send("API Working");
 });
 app.post("/webhooks", clerkWebhooks);
 
+
 process.on("uncaughtException", (err) => {
   Sentry.captureException(err);
   console.error("Uncaught Exception:", err);
 });
+
 process.on("unhandledRejection", (err) => {
   Sentry.captureException(err);
   console.error("Unhandled Rejection:", err);
 });
 
-let isDbConnected = false;
 
+let isDbConnected = false;
 const ensureDbConnection = async () => {
   if (!isDbConnected) {
     await connectDB();
@@ -37,25 +41,26 @@ const ensureDbConnection = async () => {
 };
 
 
-if (process.env.LOCAL === "true") {
+const isLocal = process.env.LOCAL && process.env.LOCAL.toLowerCase() === "true";
+
+if (isLocal) {
+ 
   (async () => {
     await ensureDbConnection();
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () =>
-      console.log(`Local server running on http://localhost:${PORT}`)
-    );
+    app.listen(PORT, () => console.log(`Local server running on http://localhost:${PORT}`));
   })();
 }
 
 
 const handler = serverless(async (req, res) => {
   try {
-    await ensureDbConnection(); 
-    return app(req, res);
+    await ensureDbConnection();
+    return app(req, res); 
   } catch (err) {
     Sentry.captureException(err);
-    console.error("Serverless handler error:", err);
-    res.status(500).json({ success: false, message: "Server error" });
+    console.error("Server error:", err);
+    return res.status(500).json({ success: false, message: "Server error" });
   }
 });
 
