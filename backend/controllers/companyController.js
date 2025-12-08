11c05c62a2896ee4223c7a359facfe09249b2e07
NataliaIancovscaia <Natalia.Iancovscaia@medieinstitutet.se>
2025-12-08 12:08:40 +0100

    Added logik i controllers for login company

--- backend/controllers/companyController.js
+++ backend/controllers/companyController.js
@@ -52,6 +52,30 @@ export const registerCompany=async(req,res)=>{
 
 //Company logo
 export const loginCompany=async(req,res)=>{
+    const {email,password}=req.body;
+    try {
+        const company=await Company.findOne({email});
+        if(bcrypt.compare(password,company.password)){
+
+            res.json({
+                succes:true,
+                company:{
+                _id:company._id,
+                name:company.name,
+                email:company.email,
+                image:company.image
+
+
+            },
+            token:generateToken(company._id)
+            })
+
+        }else{
+            res.json({success:false,message:'Invalid email or password'})
+        }
+    } catch (error) {
+         res.json({success:false,message:error.message})
+    }
 
 }
 
